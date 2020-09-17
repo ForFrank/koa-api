746e866eea432eb9d1760568179318eb90e7809f
frank <liangzhenqi@hotmail.com>
2020-09-17 23:49:19 +0800

    update

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -1,10 +1,13 @@
+const path =require('path')
 class HomeCtl {
     index(ctx) {
         ctx.body = '这是主页';
     }
     upload(ctx){
-        const file=ctx.request.files;
-        ctx.body={path: file};
+        const file=ctx.request.files.file;
+        const basename =path.basename(file.path);
+        console.log(basename)
+        ctx.body={url: ` ${ctx.origin}/uploads/${basename}`};
     }
 }
 
--- app/index.js
+++ app/index.js
@@ -1,5 +1,6 @@
 const Koa = require('koa');
 const koaBody = require('koa-body');
+const koaStatic = require('koa-static');
 const error = require('koa-json-error');
 const parameter = require('koa-parameter');
 const mongoose = require('mongoose')
@@ -11,12 +12,13 @@ const { connectionStr } = require('./config');
 mongoose.connect(connectionStr, { useUnifiedTopology: true }, { useNewUrlParser: true }, () => console.log('MongoDB连接成功了！'));
 mongoose.connection.on('error', console.error);
 
+app.use(koaStatic(path.join(__dirname,'./public')))
 app.use(error({
     postFormat: (e, { stack, ...rest }) => process.env.NODE_ENV === 'production' ? rest : { stack, ...rest }
 }));
 app.use(koaBody({
     multipart: true,
-    formLimit: {
+    formidable: {
         uploadDir: path.join(__dirname, './public/uploads'),
         keepExtensions: true,
     },
